74309256de9e38a49cf2bf3cbeab82ad700ccd85
binayrawat <binayrawat8@gmail.com>
2025-02-23 02:50:50 +0100

    Deploy

--- src/processor/process.py
+++ src/processor/process.py
@@ -33,4 +33,3 @@ def process_recipe(bucket, input_key, output_key):
             os.remove("/tmp/input.file")
         if os.path.exists("/tmp/output.zip"):
             os.remove("/tmp/output.zip")
-
